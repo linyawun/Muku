225c33366ec08eaa5322db735d84d91d093d7a28
linyawun <linyawun031@gmail.com>
2023-06-26 17:01:48 +0800

    update CTA buttom

--- src/pages/front/Home.js
+++ src/pages/front/Home.js
@@ -56,7 +56,7 @@ function Home() {
                 <div className='bg-white opacity-50 w-100 h-100 position-absolute top-0 start-0 w-100 h-100'></div>
                 <button
                   type='button'
-                  className='btn btn-primary btn-lg p-3 px-4 position-absolute CTA'
+                  className='btn btn-primary btn-lg position-absolute CTA'
                 >
                   前往購物
                 </button>
@@ -68,7 +68,7 @@ function Home() {
                 <div className='bg-white opacity-50 w-100 h-100 position-absolute top-0 start-0 w-100 h-100'></div>
                 <button
                   type='button'
-                  className='btn btn-primary btn-lg p-3 px-4 position-absolute CTA'
+                  className='btn btn-primary btn-lg position-absolute CTA'
                 >
                   前往購物
                 </button>
