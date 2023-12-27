a85c3c61fa04d157a1b7867855d2bbfebf9ba70a
linyawun <linyawun031@gmail.com>
2023-12-27 23:33:26 +0800

    perf: add lightningcss and vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'gzip', // 或者使用 'brotliCompress' 用於Brotli壓縮
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -13,5 +19,7 @@ export default defineConfig({
   },
   build: {
     target: browserslistToEsbuild(),
+    minify: 'terser',
+    cssMinify: 'lightningcss',
   },
 });
